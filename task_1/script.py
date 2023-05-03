3e5da6ebdef08b6e4730abbf2a5cf43fa2358322
numo <zybin228@gmail.com>
2023-05-03 15:29:02 +0300

    removed output to file

--- task_1/script.py
+++ task_1/script.py
@@ -2,16 +2,16 @@ from pathlib import Path
 
 
 def main():
-    dirpath = Path('/srv/example/foo/bar')
-    filenames = [f.name[:-4] + '\n' for f in dirpath.rglob("*.csv")]
     try:
-        with open('csv_files.txt', 'w') as f:
-            f.writelines(filenames)
-    except IOError:
-        print('Can\'t write result to the file.')
-        print('Result:')
-        print(*filenames, sep='', end='')
+        path = input('Enter path to the folder: ')
+        dirpath = Path(path)
+        filenames = [f.name[:-4] + '\n' for f in dirpath.rglob("*.csv")]
+    except:
+        print('Incorrect path to the folder.')
 
+    print('\nResult:')
+    print(*filenames, sep='', end='')
+        
     
 if __name__ == "__main__":
     main()
